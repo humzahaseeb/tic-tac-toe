735b3a2f64ead03132dd0f1d1152f4f87d436e87
hamza haseeb <hamzahaseeb5@gmail.com>
2022-02-04 09:23:44 +0500

    commented out eslint disable rule

--- src/Board.jsx
+++ src/Board.jsx
@@ -27,7 +27,7 @@ const Board = () => {
             clearGame();
             alert(`Ta da ! ${winner} won the Game !`)
         }
-    }, [gameState])
+    }, [gameState]) // eslint-disable-line react-hooks/exhaustive-deps
 
     const checkWinner = () => {
         const winningCombinations = [
