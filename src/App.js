886e0827b6fb4bc819d988a9e1888459c7d524a3
hamza haseeb <hamzahaseeb5@gmail.com>
2022-02-04 06:48:09 +0500

    basic styling done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className='app-header'>
+        <h1 className='heading-text'>Tic Tac Toe</h1>
     </div>
     );
 };
